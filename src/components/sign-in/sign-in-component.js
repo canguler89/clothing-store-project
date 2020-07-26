db1e8b3aae8f6a82d12290c7a91b90c2f27b0d66
Can Muhammet GULER <canmuhammetguler@Cans-MacBook-Pro.local>
2020-07-26 12:48:38 -0500

    custom button added

--- src/components/sign-in/sign-in-component.js
+++ src/components/sign-in/sign-in-component.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button";
 
 class signIn extends Component {
   constructor(props) {
@@ -45,7 +46,7 @@ class signIn extends Component {
             required
           />
 
-          <input type="submit" value="Submit Form" />
+          <CustomButton type="submit"> Sign in </CustomButton>
         </form>
       </div>
     );
