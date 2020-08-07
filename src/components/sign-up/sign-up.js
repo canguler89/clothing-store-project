7c5d06759daab2b71b4db28f80ee0b067e8b047d
Can Guler <canmuhammetguler@gmail.com>
2020-08-06 21:02:17 -0500

    sign up component

--- src/components/sign-up/sign-up.js
+++ src/components/sign-up/sign-up.js
@@ -17,6 +17,29 @@ class SignUp extends React.Component {
       confirmPassword: "",
     };
   }
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { displayName, email, password, confirmPassword } = this.state;
+    if (password !== confirmPassword) {
+      alert("passwords don't match");
+      return;
+    }
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      createUserProfileDocument(user, { displayName });
+      this.setState({
+        displayName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   render() {
     const { displayName, email, password, confirmPassword } = this.state;
@@ -32,9 +55,36 @@ class SignUp extends React.Component {
             onChange={this.handleChange}
             label="Display Name"
             required
-          ></FormInput>
+          />
+          <FormInput
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+            label="Email"
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+            label="password"
+            required
+          />
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={this.handleChange}
+            label="Confirm Password"
+            required
+          />
+          <CustomButton type="submit">SIGN UP </CustomButton>
         </form>
       </div>
     );
   }
 }
+
+export default SignUp;
