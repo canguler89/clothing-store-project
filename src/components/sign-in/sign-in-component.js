9b356e7844f07c637ce7ec6cc2240a5a62d31ff5
Can Guler <canmuhammetguler@gmail.com>
2020-08-07 01:49:16 -0500

    finished sign in and sing up

--- src/components/sign-in/sign-in-component.js
+++ src/components/sign-in/sign-in-component.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class signIn extends Component {
   constructor(props) {
@@ -13,8 +13,15 @@ class signIn extends Component {
     };
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault();
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.error(error);
+    }
 
     this.setState({ email: "", password: "" });
   };
